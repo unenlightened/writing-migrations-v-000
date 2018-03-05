12266faf0e346ff62431cdb4a15f2028873edc5c
Chely Ho <adept_inept@yahoo.com>
2018-03-05 18:06:16 +0000

    automatically backed up by learn

--- db/migrate/03_change_datatype_for_birthday.rb
+++ db/migrate/03_change_datatype_for_birthday.rb
@@ -1,5 +1,6 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[4.2]
   def change
+    add_column :students, :grade, :integer
     change_column :students, :birthday, :datetime
   end
 end
